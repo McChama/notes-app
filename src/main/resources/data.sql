bc6fbd39a172f13fdd4208e3252a1db4fb108ad9
mcchama <aoe2_emi@hotmail.com>
2021-12-23 02:12:11 -0600

    Database configuration changed

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO NOTE VALUES (1, "Note Title 1", "Note content 1", "12:00:01", "12:00:01");
+INSERT INTO NOTE VALUES (2, "Note Title 2", "Note content 2", "12:00:02", "12:00:02");
+INSERT INTO NOTE VALUES (3, "Note Title 3", "Note content 3", "12:00:03", "12:00:03");
+INSERT INTO NOTE VALUES (4, "Note Title 4", "Note content 4", "12:00:04", "12:00:04");
+INSERT INTO NOTE VALUES (5, "Note Title 5", "Note content 5", "12:00:05", "12:00:05");
